c81e368025cc48e82689d9643116d99821ade5a8
Uttam <ushar3615@gmail.com>
2024-07-18 08:28:30 +0530

    Done ||

---
+++ src/components/About.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+          Hello, I'm Uttam , a passionate Full Stack developer with a keen eye for MERN
+          Stack . With a background in IT, I strive to create impactful and
+          visually stunning Software solutions that leave a lasting impression.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+          Bachelor Of Technology (B.Tech) <br/> <b>Guru Gobind Singh Indraprastha University (Delhi)</b> [2020-2024] <br/>
+          Computer Science And Engineering
+          
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          Proficient in <b> Java Programming </b>Experienced with <b> VS Code, Eclipse,   NeatBean, Intellij </b> Idea Strong grasp of [Design Principles/Concepts]
+          Excellent problem-solving skills Effective communicator and
+          collaborator
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Professional Experience
+        </h1>
+        <span>
+          Full Stack Developer Intern  [Obscura]<br/> May 2023 - July 2023 <br/>Worked as a MERN Stack Developer And Developed a reastaurent Rating Project
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Achievements & Awards
+        </h1>
+        <span>
+            200+ LeetCode Problems and earned 50 Days Badges on leetcode
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my skills and creativity to deliver
+          innovative [Software Development] solutions that exceed client expectations and
+          contribute positively to the digital landscape. I am committed to
+          continuous learning and growth, always seeking new challenges and
+          opportunities to expand my horizons.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
